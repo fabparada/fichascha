2725c78c6b6a5bb45a0a83d0e1115cff7bf2fba9
Fabián Parada Fernández <fparada@minsegpres.cl>
2019-07-31 16:03:25 -0400

    Primer commit

---
+++ src/components/Galeria.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="galeria">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: "este componente en una galeria"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
